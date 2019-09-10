752a9c790b2a958b28d922e90970e05858935e9d
shyama95 <shyamap95@gmail.com>
2019-09-10 19:55:47 +0530

    Added barcode/qr reading, updated evaluation algorithm

--- omr.py
+++ omr.py
@@ -2,7 +2,10 @@ import argparse
 import cv2
 import math
 import numpy as np
-
+from pyzbar import pyzbar
+from os import walk
+import os
+import numpy as np
 
 CORNER_FEATS = (
     0.322965313273202,
@@ -11,7 +14,7 @@ CORNER_FEATS = (
     0.998754685666376,
 )
 
-TRANSF_SIZE = 512
+TRANSF_SIZE = 1024
 
 
 def normalize(im):
@@ -25,7 +28,6 @@ def get_approx_contour(contour, tol=.01):
 def get_contours(image_gray):
     im2, contours, hierarchy = cv2.findContours(
         image_gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
     return map(get_approx_contour, contours)
 
 def get_corners(contours):
@@ -117,21 +119,21 @@ def perspective_transform(img, points):
 
 def sheet_coord_to_transf_coord(x, y):
     return list(map(lambda n: int(np.round(n)), (
-        TRANSF_SIZE * x/744.055,
-        TRANSF_SIZE * (1 - y/1052.362)
+        TRANSF_SIZE * x/740,
+        TRANSF_SIZE * (1 - y/1230)
     )))
 
 def get_question_patch(transf, q_number):
     # Top left
     tl = sheet_coord_to_transf_coord(
         200,
-        850 - 80 * (q_number - 1)
+        1030 - 100 * (q_number - 1)
     )
 
     # Bottom right
     br = sheet_coord_to_transf_coord(
         650,
-        800 - 80 * (q_number - 1)
+        980 - 100 * (q_number - 1)
     )
     return transf[tl[1]:br[1], tl[0]:br[0]]
 
@@ -149,19 +151,20 @@ def draw_marked_alternative(question_patch, index):
     cx, cy = sheet_coord_to_transf_coord(
         50 * (2 * index + .5),
         50/2)
-    draw_point((cx, TRANSF_SIZE - cy), question_patch, radius=5, color=(255, 0, 0))
+    draw_point((cx, TRANSF_SIZE - cy), question_patch, radius=5, color=(0, 255, 0))
 
 def get_marked_alternative(alternative_patches):
     means = list(map(np.mean, alternative_patches))
     sorted_means = sorted(means)
     
-    print(sorted_means[0]/sorted_means[1])
+    # print(means)
+    # print(sorted_means[0]/sorted_means[1])
     
-    for val in alternative_patches:
-        print(val)
-
-    # Simple heuristic
-    if sorted_means[0]/sorted_means[1] > .85:
+    # Eliminate no answers marked case
+    if sorted_means[0] > 200:
+        return None
+    # Eliminate double marking case
+    elif sorted_means[0] < 200 and sorted_means[1] < 200:
         return None
 
     return np.argmin(means)
@@ -169,7 +172,7 @@ def get_marked_alternative(alternative_patches):
 def get_letter(alt_index):
     return ["A", "B", "C", "D", "E"][alt_index] if alt_index is not None else "N/A"
 
-def get_answers(source_file):
+def get_answers(source_file, file):
     """Run the full pipeline:
 
         - Load image
@@ -185,22 +188,25 @@ def get_answers(source_file):
 
     im_orig = cv2.imread(source_file)
 
-    blurred = cv2.GaussianBlur(im_orig, (11, 11), 10)
+    blurred = cv2.GaussianBlur(im_orig, (9, 9), 10)
+
+    cv2.imwrite("../test_images/b_blur.png", blurred)
 
     im = normalize(cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY))
 
-    ret, im = cv2.threshold(im, 127, 255, cv2.THRESH_BINARY)
+    ret, im = cv2.threshold(im, 200, 255, cv2.THRESH_BINARY)
     
-    cv2.imwrite("../test_images/b_th.png", im)
+    # cv2.imwrite("../test_images/b_th1.png", im)
+    cv2.imwrite("tmp/tmp_"+file, im)
+    imNorm = im.copy()
 
     contours = get_contours(im)
-    corners = get_corners(contours)
 
-    cv2.drawContours(im_orig, corners, -1, (0, 255, 0), 3)
+    corners = get_corners(contours)
 
     outmost = order_points(get_outmost_points(corners))
 
-    transf = perspective_transform(im_orig, outmost)
+    transf = perspective_transform(imNorm, outmost)
 
     answers = []
     for i, q_patch in enumerate(get_question_patches(transf)):
@@ -216,6 +222,31 @@ def get_answers(source_file):
     #cv2.imshow('bw', im)
 
     return answers, transf
+
+def get_uid(image):
+   # image = cv2.imread(source_file)
+   barcodes = pyzbar.decode(image) 
+   barcodeData = "" 
+   
+   # loop over the detected barcodes
+   for barcode in barcodes:
+        # extract the bounding box location of the barcode and draw the
+        # bounding box surrounding the barcode on the image
+        (x, y, w, h) = barcode.rect
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+        # the barcode data is a bytes object so if we want to draw it on
+        # our output image we need to convert it to a string first
+        barcodeData = barcode.data.decode("utf-8")
+        barcodeType = barcode.type
+
+        # draw the barcode data and barcode type on the image
+        text = "{} ({})".format(barcodeData, barcodeType)
+        cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (0, 0, 255), 2)
+        cv2.imwrite("../test_images/0out.png", image)
+
+   return barcodeData 
 
 def main():
     parser = argparse.ArgumentParser()
@@ -238,21 +269,57 @@ def main():
 
     args = parser.parse_args()
 
-    answers, im = get_answers(args.input)
+    splitImageCmd = "if [ -e tmp ]; then rm -rf tmp; fi && mkdir tmp && convert -density 300 -depth 8 -quality 100 " + args.input + " tmp/answer_sheet.png"
+    os.system(splitImageCmd)
 
-    for i, answer in enumerate(answers):
-        print("Q{}: {}".format(i + 1, answer))
+    answerKey = np.loadtxt(open("answerkey.csv", "r"), delimiter=",", dtype = 'str')    
+    answerKey = answerKey[:,1]
+    
+    for (dirpath, dirnames, filenames) in walk("tmp/"):
+        files = filenames
+     
+    for file in files:
+        imagePath = "tmp/" + file
+        image = cv2.imread(imagePath) 	
+        
+	# uid = get_uid(imagePath)        
+        uid = get_uid(image)
 
-    if args.output:
-        cv2.imwrite(args.output, im)
-        print("Wrote image to {}".format(args.output))
+        if(uid == ''):
+            # print("Barcode not read")
 
-    if args.show:
-        cv2.imshow('trans', im)
+            blurred = cv2.GaussianBlur(image, (9, 9), 10)
+            image = normalize(cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY))
+            ret, image = cv2.threshold(image, 200, 255, cv2.THRESH_BINARY)
+            cv2.imwrite("../test_images/out_" + file + "_tmp.png", image)
+                                                                     
+            uid = get_uid(image)
+            
 
-        print("Close image window and hit ^C to quit.")
-        while True:
-            cv2.waitKey()
+        if(1):
+            print("ID : {}".format(uid))
+
+            answers, im = get_answers(imagePath, file)
+            score = 0
+
+            for i, answer in enumerate(answers):
+                solution = answerKey[i]
+
+                print("Q{}: {}".format(i + 1, answer))
+                # print(solution)
+
+                if(answer == solution):
+                    score += 10
+            
+            print("Socre = {}".format(score))
+            cv2.imwrite("tmp/out_"+file, im)
+            print("Wrote image to {}".format("tmp/out_"+file))
+
+            if args.show:
+               cv2.imshow('trans', im)
+
+        else:
+            print("Failed to read unique id")
 
 if __name__ == '__main__':
     main()
